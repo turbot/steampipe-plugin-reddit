4b7f15d4d75fe1489610df542504e4916f97fcb8
Mike Keller <mike.k@blu-web.com>
2023-10-22 17:16:19 -0700

    Add reddit_my_saved_post and reddit_my_saved_comment tables

---
+++ docs/tables/reddit_my_saved_post.md
@@ -0,0 +1,67 @@
+# Table: reddit_my_saved_post
+
+Query your saved posts.
+
+Note:
+When querying the `reddit_my_saved_post` or `reddit_my_saved_comment` tables, be aware that the underlying library fetches both saved posts and comments together from the Reddit API, with a default request of 100 items. Each query, however, will only return one type of item—either posts or comments—based on the table you are querying. Due to this behavior, the number of items returned in your query might be less than the total number of saved items you have, or the default request amount of 100, especially if there is a significant mix of saved posts and comments. The SQL `LIMIT` clause will only apply to the data returned by the library, not the data requested from the Reddit API.
+
+## Examples
+
+### 5 most recent posts
+
+```sql
+select
+  created_utc,
+  title,
+  url
+from
+  reddit_my_saved_post
+order by
+  created_utc desc
+limit 5;
+```
+
+### Top 5 posts by score
+
+```sql
+select
+  score,
+  upvote_ratio,
+  title,
+  url
+from
+  reddit_my_saved_post
+order by
+  score desc
+limit 5;
+```
+
+### Posts by subreddit
+
+```sql
+select
+  subreddit_name_prefixed,
+  count(*)
+from
+  reddit_my_saved_post
+group by
+  subreddit_name_prefixed
+order by
+  count desc;
+```
+
+### Posts containing the word "docs"
+
+```sql
+select
+  created_utc,
+  title,
+  url,
+  selftext
+from
+  reddit_my_saved_post
+where
+  selftext ilike '%docs%'
+order by
+  created_utc;
+```
---
+++ reddit/table_reddit_my_saved_post.go
@@ -0,0 +1,107 @@
+package reddit
+
+import (
+	"context"
+
+	"github.com/vartanbeno/go-reddit/v2/reddit"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+)
+
+func tableRedditMySavedPost(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "reddit_my_saved_post",
+		Description: "Your saved posts.",
+		List: &plugin.ListConfig{
+			Hydrate: listMySavedPost,
+		},
+		Columns: []*plugin.Column{
+			// Top columns
+			{Name: "rank", Type: proto.ColumnType_INT, Description: "Rank of the post among the result rows, use for sorting."},
+			{Name: "id", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.ID"), Description: "ID of the post."},
+			{Name: "name", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.FullID"), Description: "Slug (full ID) of the post."},
+			{Name: "created_utc", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Post.Created").Transform(timeToRfc3339), Description: "Time when the post was created."},
+			{Name: "edited", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Post.Edited").Transform(timeToRfc3339), Description: "Time when the post was edited."},
+			{Name: "permalink", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.Permalink"), Description: "Permalink (path only) to the post."},
+			{Name: "url", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.URL"), Description: "URL the post links to, or of the post itself."},
+			{Name: "title", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.Title"), Description: "Title of the post."},
+			{Name: "selftext", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.Body"), Description: "Body of the post."},
+			{Name: "likes", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.Likes"), Description: "True if you've upvoted the post. False if you've downvoted it. Otherwise null."},
+			{Name: "score", Type: proto.ColumnType_INT, Transform: transform.FromField("Post.Score"), Description: "Score of the post."},
+			{Name: "upvote_ratio", Type: proto.ColumnType_DOUBLE, Transform: transform.FromField("Post.UpvoteRatio"), Description: "Upvote ratio of the post."},
+			{Name: "num_comments", Type: proto.ColumnType_INT, Transform: transform.FromField("Post.NumberOfComments"), Description: "Number of comments on the post."},
+			{Name: "subreddit", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.SubredditName"), Description: "Name of the subreddit, e.g. aws."},
+			{Name: "subreddit_name_prefixed", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.SubredditNamePrefixed"), Description: "Prefixed name of the subreddit, e.g. /r/aws."},
+			{Name: "subreddit_id", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.SubredditID"), Description: "ID of the subreddit."},
+			{Name: "subreddit_subscribers", Type: proto.ColumnType_INT, Transform: transform.FromField("Post.SubredditSubscribers"), Description: "Number of subscribers to the subreddit."},
+			{Name: "author", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.Author"), Description: "Author of the post."},
+			{Name: "author_fullname", Type: proto.ColumnType_STRING, Transform: transform.FromField("Post.AuthorID"), Description: "Full name of the author for the post."},
+			{Name: "spoiler", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.Spoiler"), Description: "True if the post is a spoiler."},
+			{Name: "locked", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.Locked"), Description: "True if the post is locked."},
+			{Name: "over_18", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.NSFW"), Description: "True if the post is not safe for work (over 18)."},
+			{Name: "is_self", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.IsSelfPost"), Description: ""},
+			{Name: "saved", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.Saved"), Description: "True if the post has been saved."},
+			{Name: "stickied", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Post.Stickied"), Description: "True if the post has been stickied."},
+		},
+	}
+}
+
+type mySavedPostRow struct {
+	Post *reddit.Post
+	// Return rank to sort results
+	Rank int `json:"rank"`
+}
+
+func listMySavedPost(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+
+	conn, err := connect(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("reddit_my_saved_post.listMySavedPost", "connection_error", err)
+		return nil, err
+	}
+
+	authUserCached := plugin.HydrateFunc(getRedditAuthenticatedUser).WithCache()
+	commonData, _ := authUserCached(ctx, d, h)
+	authUser := commonData.(string)
+
+	if authUser != "" {
+		conn.Username = authUser
+	}
+
+	opts := &reddit.ListUserOverviewOptions{
+		ListOptions: reddit.ListOptions{
+			Limit: 100,
+		},
+		// One of: hot, new, top, controversial.
+		Sort: "top",
+		// One of: hour, day, week, month, year, all.
+		Time: "all",
+	}
+
+	count := 0
+	for {
+		items, _, resp, err := conn.User.Saved(ctx, opts)
+		if err != nil {
+			plugin.Logger(ctx).Error("reddit_my_post.listMySavedPost", "query_error", err, "resp", resp, "opts", opts)
+			return nil, err
+		}
+		for _, i := range items {
+			count++
+			row := mySavedPostRow{
+				i,
+				count,
+			}
+			d.StreamListItem(ctx, row)
+		}
+		// Stop if we've reached the end, or the max target limit
+		if resp.After == "" {
+			break
+		}
+		// Set the page and continue
+		opts.After = resp.After
+	}
+
+	return nil, nil
+}
