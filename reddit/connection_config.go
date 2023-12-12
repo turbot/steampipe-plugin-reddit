f65801029e8182494d6bd2f91648e27fcc0fd2f6
Madhushree Ray <madhushree@turbot.com>
2023-12-13 01:40:30 +0530

    Remove config schema

--- reddit/connection_config.go
+++ reddit/connection_config.go
@@ -2,33 +2,14 @@ package reddit
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type redditConfig struct {
-	ClientID     *string `cty:"client_id"`
-	ClientSecret *string `cty:"client_secret"`
-	Username     *string `cty:"username"`
-	Password     *string `cty:"password"`
-	AccessToken  *string `cty:"access_token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"client_id": {
-		Type: schema.TypeString,
-	},
-	"client_secret": {
-		Type: schema.TypeString,
-	},
-	"username": {
-		Type: schema.TypeString,
-	},
-	"password": {
-		Type: schema.TypeString,
-	},
-	"access_token": {
-		Type: schema.TypeString,
-	},
+	ClientID     *string `hcl:"client_id"`
+	ClientSecret *string `hcl:"client_secret"`
+	Username     *string `hcl:"username"`
+	Password     *string `hcl:"password"`
+	AccessToken  *string `hcl:"access_token"`
 }
 
 func ConfigInstance() interface{} {
